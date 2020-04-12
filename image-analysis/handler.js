7e636591856022c86651506a376c64399bbc7c03
Cauê Kotarski <caue1017@hotmail.com>
2020-04-11 21:19:14 -0300

    Create translated text

--- image-analysis/handler.js
+++ image-analysis/handler.js
@@ -1,52 +1,76 @@
-'use strict';
-const { promises: { readFile } } = require('fs');
+"use strict";
+const {
+  promises: { readFile },
+} = require("fs");
 
 class Handler {
-  constructor({ rekoSvc }) {
+  constructor({ rekoSvc, translatorSvc }) {
     this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
   }
 
   async detectImageLabels(buffer) {
-    const result = await this.rekoSvc.detectLabels({
-      Image: { 
-        Bytes: buffer
-      }
-    }).promise();
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
 
-    const workingItems = result.Labels
-      .filter(({ Confidence }) => Confidence > 80);
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 80
+    );
 
-    const names = workingItems
-      .map(({ Name }) => Name)
-      .join(' and ');
+    const names = workingItems.map(({ Name }) => Name).join(" and ");
 
     return { names, workingItems };
   }
 
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+
+    const result = await this.translatorSvc.translateText(params).promise();
+
+    console.log(JSON.stringify(result));
+  }
+
   async main(event) {
     try {
-      const imgBuffer = await readFile('./images/cat.jpg');
+      const imgBuffer = await readFile("./images/cat.jpg");
+      console.log("Detecting labels...");
       const { names, workingItems } = await this.detectImageLabels(imgBuffer);
-      console.log({ names, workingItems });
+
+      console.log("Translating to Portuguese...");
+      const texts = await this.translateText(names);
+
+      console.log('handling final object...');
+
       return {
         statusCode: 200,
-        body: 'Hello!'
-      }
+        body: "Hello!",
+      };
     } catch (error) {
-      console.log('Error***', error.stack);
+      console.log("Error***", error.stack);
       return {
         statusCode: 500,
-        body: 'Internal server error'
-      }
+        body: "Internal server error",
+      };
     }
   }
 }
 
 //factory
-const aws = require('aws-sdk');
+const aws = require("aws-sdk");
 const rekognition = new aws.Rekognition();
+const translator = new aws.Translate();
 const handler = new Handler({
-  rekoSvc: rekognition
+  rekoSvc: rekognition,
+  translatorSvc: translator,
 });
 
 module.exports.main = handler.main.bind(handler);
