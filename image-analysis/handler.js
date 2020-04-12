69fb2eecce043c7d3b2709a9f8ddd56609afd79a
Cauê Kotarski <caue1017@hotmail.com>
2020-04-11 21:03:48 -0300

    Create analysis class

--- image-analysis/handler.js
+++ image-analysis/handler.js
@@ -1,18 +1,49 @@
 'use strict';
+const { promises: { readFile } } = require('fs');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+class Handler {
+  constructor({ rekoSvc }) {
+    this.rekoSvc = rekoSvc;
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc.detectLabels({
+      Image: { 
+        Bytes: buffer
+      }
+    }).promise();
+
+    const workingItems = result.Labels
+      .filter(({ Confidence }) => Confidence > 80);
+
+    const names = workingItems
+      .map(({ Name }) => Name)
+      .join(' and ');
+  }
+
+  async main(event) {
+    try {
+      const imgBuffer = await readFile('./images/cat.jpg');
+      this.detectImageLabels(imgBuffer);
+      return {
+        statusCode: 200,
+        body: 'Hello!'
+      }
+    } catch (error) {
+      console.log('Error***', error.stack);
+      return {
+        statusCode: 500,
+        body: 'Internal server error'
+      }
+    }
+  }
+}
+
+//factory
+const aws = require('aws-sdk');
+const rekognition = new aws.Rekognition();
+const handler = new Handler({
+  rekoSvc: rekognition
+});
+
+module.exports.main = handler.main.bind(handler);
